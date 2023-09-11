249dbe31c0f1119153a717162c3d53bccc102c4e
Dmitriy Mishchenko <51910160+dmmishchenko@users.noreply.github.com>
2023-09-11 11:44:41 +0300

    Show simple message and message with buttons (#2)
    * add message type and message renderer
    
    * add message with buttons

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 var token = Environment.GetEnvironmentVariable("TOKEN")!;
 
 // start the bot
 var bot = new TelegramBotClient(token);
+var renderer = new TelegramMessageRenderer();
 var me = await bot.GetMeAsync();
 using var cts = new CancellationTokenSource();
 bot.StartReceiving(HandleUpdateAsync, PollingErrorHandler, null, cts.Token);
@@ -45,9 +47,33 @@ async Task BotMessageReceived(ITelegramBotClient bot, Message? message)
   if (message != null && message.Text != null)
   {
     var chatId = message.Chat.Id;
-    var messageText = message.Text;
 
-    // Echo the received message back to the user
-    await bot.SendTextMessageAsync(chatId, "You said: " + messageText);
+    if (message.Entities != null && message.Entities.Any(e => e.Type == MessageEntityType.Hashtag))
+    {
+      var hashTags = message.Entities
+      .Where(entity => entity.Type == MessageEntityType.Hashtag)
+      .Select(e => message.Text.Substring(e.Offset, e.Length))
+      .ToArray();
+
+      if (hashTags.Any())
+      {
+        var messageOptions = new InteractionOptions(message: message.Text, options: hashTags);
+        var interactionMessage = renderer.RenderMessage(MessageType.Interaction, messageOptions);
+
+        // Create inline keyboard markup
+        var inlineKeyboard = new InlineKeyboardMarkup(interactionMessage.Buttons);
+
+        // Create a message with the inline keyboard
+        // var message = "Choose an option:";
+        await bot.SendTextMessageAsync(chatId, interactionMessage.Message, replyMarkup: inlineKeyboard);
+      }
+    }
+
+    else
+    {
+      var messageText = message.Text;
+      // Echo the received message back to the user
+      await bot.SendTextMessageAsync(chatId, "You said: " + messageText);
+    }
   }
 }
